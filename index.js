24b9f4808a20ea46e080f9c4a1d735c84cce274f
Victor Muñoz <victormu497@gmail.com>
2021-04-04 21:41:40 -0500

    Servidor inicial creado

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+
+app.get('/', function(req, res) {
+    res.send("Hello World");
+});
+
+app.get('/json', function(req, res) {
+    res.json("Hello JSON");
+});
+
+app.listen(config.port, function() {
+    console.log(`Listening http://localhost:${config.port}`);
+});
+
