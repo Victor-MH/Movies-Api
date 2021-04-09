4ff6b98f78a2ce3184e6a8c31f8f5db300818bc4
Victor Muñoz <victormu497@gmail.com>
2021-04-08 22:33:14 -0500

    Clase 21 completa, inicio de CRUD y extra de get/:id

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
+// app.get('/', function(req, res) {
+//     res.send("Hello World");
+// });
 
-app.get('/', function(req, res) {
-    res.send("Hello World");
-});
+// app.get('/json', function(req, res) {
+//     res.json("Hello JSON");
+// });
 
-app.get('/json', function(req, res) {
-    res.json("Hello JSON");
-});
+moviesApi(app);
 
 app.listen(config.port, function() {
     console.log(`Listening http://localhost:${config.port}`);
